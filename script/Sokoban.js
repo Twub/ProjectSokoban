671d678b40cdecfbcf90ec102594fc2157e4de99
Martin Hellström <martver12@gmail.com>
2019-11-25 12:40:05 +0100

    added grid

--- script/Sokoban.js
+++ script/Sokoban.js
@@ -2,9 +2,32 @@ export default{
     template:`
     <div class="sokoban">
         <div id="grid">
-            <p>text test</p>
+            <tr v-for="grids in gridLayout">
+            <td v-for="(grids, grids) in gridLayout">*insert grid*</td>
+            </tr>
         </div>
     </div>
     `,
-
+    data(){
+        return{
+            gridLayout: [
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                []
+            ]
+        }
+    },
 }
