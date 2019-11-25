2678ba75dc3f2a9045fe11fa3687e0510a1c3d9a
Hampus Nilsson <hampus10699@gmail.com>
2019-11-25 17:44:30 +0100

    created settings tab file and imported to header js file, and added some cool shadow effects to the header and sokoban div.

--- script/header.js
+++ script/header.js
@@ -1,9 +1,27 @@
+import settingsTab from './tabs/settings.js';
+
 export default{
+    components: {
+        settingsTab,
+    },
     template: `
         <div id="header">
-            <button id="start-game-button" class="header-buttons">Start!</button>
-            <button id="settings-button" class="header-buttons">Settings</button>
-            <button id="end-game-button" class="header-buttons">Stop!</button>
+            <button id="start-game-button" class="header-buttons" v-on:click="startGame">Start!</button>
+            <button id="settings-button" class="header-buttons" v-on:click="showSettings">Settings</button>
+            <button id="end-game-button" class="header-buttons" v-on:click="stopGame">Stop!</button>
         </div>
-    `
+    `,
+    methods: {
+        startGame: function(){
+
+        },
+
+        stopGame: function(){
+
+        },
+
+        showSettings: function(){
+            
+        }
+    }
 }
---
+++ script/tabs/settings.js
@@ -0,0 +1,7 @@
+export default {
+    template: `
+        <div id="settings-tab">
+            
+        </div>
+    `
+}
