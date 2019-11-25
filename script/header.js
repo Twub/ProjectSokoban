46c918a44fde9223a35cd9ab9159464b29b22af2
Hampus Nilsson <hampus10699@gmail.com>
2019-11-25 10:49:10 +0100

    added button to header div.

--- script/header.js
+++ script/header.js
@@ -1,7 +1,9 @@
 export default{
     template: `
         <div id="header">
-            
+            <button id="start-game-button">Start!</button>
+            <button id="settings-button">Settings</button>
+            <button id="end-game-button">Stop!</button>
         </div>
     `
 }
