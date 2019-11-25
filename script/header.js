ff9ef59f2a2fe3c5539fd9fecd4d638d2efe67e6
Martin Hellström <martver12@gmail.com>
2019-11-25 10:52:41 +0100

    removed poop

--- script/header.js
+++ script/header.js
@@ -1,13 +1,9 @@
 export default{
     template: `
         <div id="header">
-<<<<<<< Updated upstream
             <button id="start-game-button">Start!</button>
             <button id="settings-button">Settings</button>
             <button id="end-game-button">Stop!</button>
-=======
-            <h1>Hello world</h1>
->>>>>>> Stashed changes
         </div>
     `
 }
