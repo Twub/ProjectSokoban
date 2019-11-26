6e64d05322efd02725620f1ad301ecf0cadb744c
Martin Hellström <martver12@gmail.com>
2019-11-26 11:18:19 +0100

    Added gridArray manipulation test

--- script/Sokoban.js
+++ script/Sokoban.js
@@ -9,11 +9,12 @@ export default{
     <div class="difficulty">
     <label for="mapSelect" id="text">Difficulty: </label>
     <select id="mapSelect" v-model="choice">
-    <option value="Nasy">Easy</option>
+    <option value="Easy">Easy</option>
     <option value="Normal">Normal</option>
     <option value="Hard">Hard</option>
     <option value="Extreme">Extreme</option>
     </select>
+    <button type="button" id="tileTest" @click="diffTest">Load difficulty</button>
     </div>
         <div id="grid">
             <tr v-for="grids in gridLayoutAngleX" id="grids">
@@ -22,15 +23,29 @@ export default{
             </td>
             </tr>
         </div>
+        <button type="button" @click="tileTest" id="tileTest">Tile test</button>
     </div>
     `,
     data(){
         return{
             gridLayoutAngleX: new Array(20),
-            gridLayoutAngleY: new Array(32)
+            gridLayoutAngleY: new Array(32),
+            choice: [
+                {Easy: 1}
+            ]
         }
     },
     methods:{
-        
+        tileTest(){
+            let i = 0
+            this.gridLayoutAngleX = ["test", "test2", i++] /* Utan dessa har gridLayoutAngleX [1] [2] [3] inget värde */
+            console.log(this.gridLayoutAngleX[0])
+        },
+        diffTest(){
+            if (this.choice = "Easy") {
+                this.gridLayoutAngleX = ["test", "test2", "Test3"]
+                console.log(this.gridLayoutAngleX[0])
+            }
+        }
     }
 }
