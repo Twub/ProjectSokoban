89f248fc7f4ffb19e2ff1d895dcf486f8c1e69ac
Martin Hellström <martver12@gmail.com>
2019-11-27 13:16:06 +0100

    Removed errors in settingstab and SG

--- script/tabs/settings.js
+++ script/tabs/settings.js
@@ -1,9 +1,9 @@
 export default {
     template: `
         <div id="settings-tab">
-            <button id="disable-sound" class="settings-buttons">Disable Sound>
-            <button id="enable-sound" class="settings-buttons">Enable Sound>
-            <label id="volume-slider-title" for="volume-slider"><span>0</span>
+            <button id="disable-sound" class="settings-buttons">Disable Sound</button>
+            <button id="enable-sound" class="settings-buttons">Enable Sound</button>
+            <label id="volume-slider-title" for="volume-slider"><span>0</span></label>
             <input type="range" min="0" max="100" value="50" id="volume-slider"><span>100</span>
             <button id="save-button">Save</button>
             <button id="cancel-button">Cancel</button>
