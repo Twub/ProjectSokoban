fb891c94246a458e9476f551e77898a47afca0a5
Martin Hellström <martver12@gmail.com>
2019-11-27 12:39:02 +0100

    Reworked grid to fully functional

--- script/grid.js
+++ script/grid.js
@@ -1,9 +1,42 @@
-export default class Grid{
-    
-    constructor(id,tile, posX, posY){
-        this.id = id
-        this.tile = tile
-        this.posX = posX
-        this.posY = posY
+import Tile from './Tile.js'
+
+export default{
+    props:['flatTiles'],
+    components:{
+        Tile
+    },
+    template: `
+    <div id="grid">
+    <Tile 
+        v-for="(tile, i) of flatTiles"
+        :position="tile"
+        :key="'tile'+ i + tile.x + tile.y"
+        id="grids"></Tile>
+        
+    </div>
+    `,
+    data(){
+        return{
+            tiles:[]
+        }
+    },
+    created(){
+
+        for(let row = 0; row < 14; row++){
+            this.tiles[row] = []
+            for(let col = 0; col < 14; col++){
+                let position = {
+                    x: col,
+                    y: row
+                }
+                this.tiles[row].push(position)
+            }
+        }
+    },
+    computed:{
+        flatTiles(){
+            return this.tiles.flat()
+        }
     }
+
 }
