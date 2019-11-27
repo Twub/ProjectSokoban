493a96fc808d0d193df6af75807938e09980e7ad
Fredrik Jönsson <Fredrikjay@gmail.com>
2019-11-27 14:25:58 +0100

    arrows style

--- script/Controls/arrowKeys.js
+++ script/Controls/arrowKeys.js
@@ -1,11 +1,13 @@
 export default{
    template:`
    <div id="arrows">
-       
+   <i @click="goLeft" class="fa fa-arrow-circle-left" id="sideArrows"></i>
+   <div id="upDownArrows">   
    <i @click="goUp" class="fa fa-arrow-circle-up"></i>
    <i @click="goDown" class="fa fa-arrow-circle-down"></i>
-   <i @click="goRight" class="fa fa-arrow-circle-right"></i>
-   <i @click="goLeft" class="fa fa-arrow-circle-left"></i>
+   </div>
+   <i @click="goRight" class="fa fa-arrow-circle-right" id="sideArrows"></i>
+   
    </div>
   
    `, 
