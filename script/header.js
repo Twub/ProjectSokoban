e05fcbd68bc2c5f10cf8376a14a763b0b3106391
Hampus Nilsson <hampus10699@gmail.com>
2019-11-27 09:44:41 +0100

    added labels and styling to settings tab.

--- script/header.js
+++ script/header.js
@@ -6,9 +6,14 @@ export default{
     },
     template: `
         <div id="header">
-            <button id="start-game-button" class="header-buttons" v-on:click="startGame">Start!</button>
-            <button id="settings-button" class="header-buttons" v-on:click="showSettings">Settings</button>
-            <button id="end-game-button" class="header-buttons" v-on:click="stopGame">Stop!</button>
+            <div id="menu-bar">
+                <button id="start-game-button" class="header-buttons" v-on:click="startGame">Start!</button>
+                <button id="settings-button" class="header-buttons" v-on:click="showSettings">Settings</button>
+                <button id="end-game-button" class="header-buttons" v-on:click="stopGame">Stop!</button>
+            </div>
+            <div id="tab">
+                <settingsTab id="settings-tab" hidden></settingsTab>
+            </div>
         </div>
     `,
     methods: {
@@ -21,7 +26,10 @@ export default{
         },
 
         showSettings: function(){
-            document.getElementById("sokobanGrid").style.visibility = 'hidden'; // bara för test just nu ska inte använda mig av denna implementation senare.
+         //   document.getElementById("sokobanGrid").style.visibility = 'hidden'; // bara för test just nu ska inte använda mig av denna implementation senare.
+            document.getElementById("settings-tab").style.display = 'block';
         }
     }
 }
+
+// <settingsTab id="settings-tab" hidden></settingsTab>
