0d7cd328a0cfd91e57b344aa9810cd8050afa23a
Hampus Nilsson <hampus10699@gmail.com>
2019-11-27 14:17:36 +0100

    added methods and binded variables to html elemnt in header file.

--- script/header.js
+++ script/header.js
@@ -1,32 +1,25 @@
 import settingsTab from './tabs/settings.js';
-
+// Enkapsulera det du vill göma/visa i tag och sen använd css för att hide/show -->
 export default{
     components: {
         settingsTab,
     },
     template: `
         <div id="header">
-<<<<<<< HEAD
             <div id="menu-bar">
                 <button id="start-game-button" class="header-buttons" v-on:click="startGame">Start!</button>
                 <button id="settings-button" class="header-buttons" v-on:click="showSettings">Settings</button>
                 <button id="end-game-button" class="header-buttons" v-on:click="stopGame">Stop!</button>
             </div>
             <div id="tab">
-                <settingsTab id="settings-tab" hidden></settingsTab> // Enkapsulera det du vill göma/visa i tag och sen använd css för att hide/show -->
+                <settingsTab id="settings-tab" hidden></settingsTab> 
             </div>
-=======
-            <button id="start-game-button" class="header-buttons" v-on:click="startGame">Start!</button>
-            <button id="settings-button" class="header-buttons" v-on:click="showSettings">Settings</button>
-            <button id="end-game-button" class="header-buttons" v-on:click="stopGame">Stop!</button>
-            <settingsTab></settingsTab> <!-- Enkapsulera det du vill göma/visa i tag och sen använd css för att hide/show -->
->>>>>>> developer
         </div>
 
     `,
     methods: {
         startGame: function(){
-
+        
         },
 
         stopGame: function(){
@@ -36,6 +29,7 @@ export default{
         showSettings: function(){
          //   document.getElementById("sokobanGrid").style.visibility = 'hidden'; // bara för test just nu ska inte använda mig av denna implementation senare.
             document.getElementById("settings-tab").style.display = 'block';
+            document.getElementById("sokobanGrid").style.display = 'none';
         }
     }
 }
--- script/tabs/settings.js
+++ script/tabs/settings.js
@@ -1,4 +1,9 @@
+import settingsManager from './settingsManager.js'
+
 export default {
+    components: {
+        settingsManager,
+    },
     template: `
         <div id="settings-tab">
 
@@ -8,23 +13,23 @@ export default {
             <label id="sound-button-title">Disable or enable sound.</label>
 
             <br>
-            <button id="disable-sound" class="settings-buttons">Disable Sound</button>
-            <button id="enable-sound" class="settings-buttons">Enable Sound</button>
+            <button id="disable-sound" class="settings-buttons" v-model="isSoundEnable" v-on:click="isSoundEnable = true">Disable Sound</button>
+            <button id="enable-sound" class="settings-buttons" v-model="isSoundEnable" v-on:click="isSoundEnable = false">Enable Sound</button>
 
             <br>
             <br>
 
             <label id="volume-slider-title" for="volume-slider">Volume:</label>
             <br>
-            <span>0</span><input type="range" min="0" max="100" value="50" id="volume-slider"><span>100</span>
+            <span>0</span><input type="range" min="0" max="100" value="50" id="volume-slider" v-model="volume"><span>100</span>
 
             <br>
             <br>
 
             <label id="timer-title">Timer:</label>
             <br>
-            <button id="enable-timer" class="settings-buttons">Enable timer</button>
-            <button id="disable-timer" class="settings-buttons">Disable timer</button>
+            <button id="enable-timer" class="settings-buttons" v-model="isTimerEnable" v-on:click="isTimerEnable = true">Enable timer</button>
+            <button id="disable-timer" class="settings-buttons" v-model="isTimerEnable" v-on:click="isTimerEnable = false">Disable timer</button>
 
             <br>
             <br>
@@ -35,14 +40,26 @@ export default {
 
             <button id="cancel-button" class="settings-buttons" v-on:click="ignoreSettings">Cancel</button>
         </div>
-    `, 
+    `,
+    data: {
+        return: {
+            isSoundEnable = '',
+            isTimerEnable = '',
+            volume = '',
+        }
+    },
     methods: {
         saveSettings: function(){
             
         },
         
         ignoreSettings: function(){
-            
+            this.showSodokoGrid();
+        },
+
+        showSodokoGrid: function(){
+            document.getElementById("settings-tab").style.display = 'none';
+            document.getElementById("sokobanGrid").style.display = 'flex';
         }
 
     }
