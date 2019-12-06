c23fcc464be7b20db3339bf9500b41b9f18b92d2
Martin Hellström <martver12@gmail.com>
2019-12-06 15:59:39 +0100

    Removed 2nd arrows at bottom screen

--- script/app.js
+++ script/app.js
@@ -1,19 +1,16 @@
 import mainHead from './header.js'
 import sokobanGame from './Sokoban.js'
-import arrowKeys from './Controls/arrowKeys.js'
 
 
 export default{
     components:{
         mainHead,
         sokobanGame,
-        arrowKeys,
     },
     template: `
     <div id="content">
         <mainHead></mainHead>
         <sokobanGame></sokobanGame>
-        <arrowKeys />
     </div>
     `
 }
